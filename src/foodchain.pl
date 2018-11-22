52113ff338e7617df268657db598f40f8594795e
Brendan M <brendan.maclean94@gmail.com>
2018-11-22 13:14:41 -0800

    Simple knowledge base for foodchain. Defined predicates herbivore, carnivore for foodchain.

---
+++ src/foodchain.pl
@@ -0,0 +1,26 @@
+
+% plant(A) defines individual A as a plant
+plant(grass).
+
+% animal(A) defines individual A as an animal
+animal(lion).
+animal(buffalo).
+animal(monkey).
+animal(mouse).
+
+% eats(A, B) defines a relation such that animal A eats individual B
+eats(lion, buffalo).
+eats(lion, monkey).
+eats(monkey, mouse).
+eats(mouse, grass).
+eats(buffalo, grass).
+
+% herbivore(X) returns true if individual X eats only plants
+herbivore(X) :-
+    eats(X, Y), plant(Y),
+    not((eats(X, Z), animal(Z))).
+
+% carnivore(X) returns true if indivual X eats only animals
+carnivore(X) :-
+    eats(X, Y), animal(Y),
+    not((eats(X, Z), plant(Z))).
