bcc33873eac9e2e50a153e181b4353ebde3acecc
Brendan M <brendan.maclean94@gmail.com>
2018-11-22 13:28:22 -0800

    Stub for animal property. Implemented Malthusian growth model in population

--- src/foodchain.pl
+++ src/foodchain.pl
@@ -15,6 +15,10 @@ eats(monkey, mouse).
 eats(mouse, grass).
 eats(buffalo, grass).
 
+% property(P, A, V) is some property P of animal A with value V
+% property(weight, lion, 100).
+
+
 % herbivore(X) returns true if individual X eats only plants
 herbivore(X) :-
     eats(X, Y), plant(Y),
